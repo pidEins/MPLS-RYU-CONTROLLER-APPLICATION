0d562ce426fd7ad92d4d96ccce406dd7231570e1
pidEins <jalandharjatt@gmail.com>
2016-12-26 23:38:04 -0500

    Install ryu file

---
+++ install_ryu.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get install python-gevent python-routes python-webob python-paramiko
+sudo apt-get install python-pip 
+sudo apt-get install mininet
+sudo pip install -U netaddr six pbr
+sudo pip install ryu
